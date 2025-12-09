8ca713741aca21098668894b236a42333e299648
Asem Rashed <asemrashed002@gmail.com>
2025-12-09 16:40:13 +0600

    Tuitions-managment functionality added

--- src/pages/dashboard/tuitions-Management/TuitionsManagement.jsx
+++ src/pages/dashboard/tuitions-Management/TuitionsManagement.jsx
@@ -6,12 +6,13 @@ import { Link } from "react-router-dom";
 import { TbListDetails } from "react-icons/tb";
 import { FaRegEdit } from "react-icons/fa";
 import { RiDeleteBin6Line } from "react-icons/ri";
+import Swal from "sweetalert2";
 
 const TuitionsManagement = () => {
     const { user } = useAuth();
     const axiosSecure = useAxiosSecure();
 
-    const { data: tuitions = [], isLoading } = useQuery({
+    const { data: tuitions = [], isLoading, refetch } = useQuery({
         queryKey: ['tuitions'],
         queryFn: async () => {
             const res = await axiosSecure.get(`/tuitions`);
@@ -21,10 +22,59 @@ const TuitionsManagement = () => {
 
     if (isLoading) return <Loading />;
 
+    const handleApprove=(tuition)=>{
+        Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be approved this!",
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, approve it!",
+        }).then(async result => {
+        if (result.isConfirmed) {
+            axiosSecure.patch(`/tuitions/${tuition._id}`,{status:'approved'})
+            .then(res=>{
+                if(res.data.modifiedCount>0){
+                    refetch()
+                    Swal.fire("Approved!", "Tuition has been approved.", "success");
+                }
+            })
+            .catch(err=>{
+                Swal.fire("Failed!", "Tuition has not been approved.", "error");
+            })
+        }
+    })
+}
+    const handleReject=(tuition)=>{
+        Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be rejected this!",
+        icon: "question",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, reject it!",
+        }).then(async result => {
+        if (result.isConfirmed) {
+            axiosSecure.patch(`/tuitions/${tuition._id}`,{status:'rejected'})
+            .then(res=>{
+                if(res.data.modifiedCount>0){
+                    refetch()
+                    Swal.fire("Rejected!", "Tuition has been rejected.", "warning");
+                }
+            })
+            .catch(err=>{
+                Swal.fire("Failed!", "Tuition has not been rejected.", "error");
+            })
+        }
+    })
+}
+
     return (
         <div className="w-full p-4 md:p-8">
             <div className="flex justify-between items-center mb-8">
-                <h2 className="text-3xl font-bold text-primary">My Tuitions</h2>
+                <h2 className="text-3xl font-bold text-primary">Tuitions Management</h2>
                 <Link to="/dashboard/add-tuition" className="btn btn-primary btn-sm text-white">
                     + Add New
                 </Link>
@@ -38,9 +88,9 @@ const TuitionsManagement = () => {
                             <th className="py-4">#</th>
                             <th>Subject Info</th>
                             <th>Class</th>
-                            <th>Location</th>
                             <th>Salary</th>
                             <th>Status</th>
+                            <th>Approve</th>
                             <th>Actions</th>
                         </tr>
                     </thead>
@@ -52,25 +102,32 @@ const TuitionsManagement = () => {
                                 </td>
                             </tr>
                         ) : (
-                            tuitions.map((item, index) => (
-                                <tr key={item._id} className="hover:bg-base-200/50 transition-colors border-b border-base-200 last:border-none">
+                            tuitions.map((tuition, index) => (
+                                <tr key={tuition._id} className="hover:bg-base-200/50 transition-colors border-b border-base-200 last:border-none">
                                     <th className="font-bold">{index + 1}</th>
                                     <td>
-                                        <div className="font-bold text-lg">{item.subject}</div>
-                                        <div className="text-xs badge badge-ghost">{item.tutorGender} Tutor Pref.</div>
+                                        <div className="font-bold text-lg">{tuition.subject}</div>
+                                        <div className="text-xs badge badge-ghost">{tuition.tutorGender} Tutor Pref.</div>
                                     </td>
-                                    <td className="font-medium">{item.class}</td>
-                                    <td>
-                                        <div className="font-medium">{item.district}</div>
-                                        <div className="text-xs opacity-60 truncate max-w-[150px]">{item.division}</div>
-                                    </td>
+                                    <td className="font-medium">{tuition.class}</td>
                                     <td className="font-mono text-primary font-bold">
-                                        ৳ {item.salary}
+                                        ৳ {tuition.salary}
                                     </td>
                                     <td>
-                                        <div className={`badge ${item.status === 'pending' ? 'badge-warning' : item.status === 'approved' ? 'badge-success' : 'badge-ghost'} gap-2 text-xs font-semibold uppercase`}>
-                                            {item.status}
+                                        <div className={` ${tuition.status === 'pending' ? 'text-warning' : tuition.status === 'approved' ? 'text-success' : 'text-error'} gap-2 text-xs font-semibold uppercase`}>
+                                            {tuition.status}
                                         </div>
+                                    </td>
+                                    <td>
+                                        <button className="btn btn-success btn-xs md:btn-sm join-item text-white" 
+                                            onClick={() => handleApprove(tuition)}>
+                                            Approve
+                                        </button>
+                                        <button className="btn btn-error btn-xs md:btn-sm ml-1 join-item text-white" 
+                                            onClick={() => handleReject(tuition)}
+                                            >
+                                            Reject
+                                        </button>
                                     </td>
                                     <td>
                                         <div className="join">
